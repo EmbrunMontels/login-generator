f921afd5a5ee7170eb4814d09bd320211bf3adf7
arcta09@gmail.com <arcta09@gmail.com>
2020-11-27 08:05:46 +0100

    first step $2

--- src/test/java/geco/LoginGeneratorTest.java
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -1,10 +1,28 @@
 package geco;
 
-import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class LoginGeneratorTest{
+    LoginService loginService;
+    @Before
+    public void init(){
+        loginService = new LoginService(new String[]{"JROL","BPER","CGUR","JDU","JRAL","JRAL1"});
 
+    }
+
+
+    @Test
     public void testGenerateLoginForNomAndPrenom() {
+        LoginGenerator loginGenerator = new LoginGenerator(loginService);
+        String toTest1 = loginGenerator.generateLoginForNomAndPrenom("Durand","Paul");
+        String result = "PDUR";
+
+        //assert
+        assertEquals(result,toTest1);
+
 
     }
 }
